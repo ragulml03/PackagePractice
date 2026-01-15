c056ac9c5544cd657b248f7677dfd03077e156b6
ragulm <ragul.lakshmanan@gmail.com>
2026-01-15 15:24:31 +0530

    Initial commit

--- Sources/PackagePractice/PackagePractice.swift
+++ Sources/PackagePractice/PackagePractice.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+public struct MyStruct {
+    public init() {}
+    public func doSomething() {
+        print("ML")
+    }
+}
